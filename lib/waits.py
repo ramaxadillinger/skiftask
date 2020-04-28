fc763c216da2d95db6053a9cdd82d78c5f775413
ramaxadillinger <ramaxadillinger@gmail.com>
2020-04-28 21:30:45 +0300

    fix files

--- lib/waits.py
+++ lib/waits.py
@@ -3,8 +3,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 
-def wait_selector(self, locator, timeout):
+def wait_selector(self, locator, timeout=5):
     element = WebDriverWait(self.driver, timeout).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, locator))
+    )
+    return element
+
+
+def wait_visibility(self, locator, timeout=5):
+    element = WebDriverWait(self.driver, timeout).until(
+        EC.visibility_of((By.CSS_SELECTOR, locator))
     )
     return element
--- tests/test_reset.py
+++ tests/test_reset.py
@@ -5,10 +5,5 @@ def test_reset_form(browser):
     page = ContactUs(browser)()
     page.send_text_to_form()
     page.reset_form()
-    d = page.get_values()
-    assert d['first_name'] == ''
-    assert d['last_name'] == ''
-    assert d['email'] == ''
-    assert d['message'] == ''
 
-    import time; time.sleep(3)
+    page.check_empty_fields()
